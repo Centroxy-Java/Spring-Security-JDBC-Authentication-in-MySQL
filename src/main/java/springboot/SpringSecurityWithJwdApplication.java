642b53f28bef49af829c5fe799162dd356eb52a8
Satyanarayan Behera <43759116+Satyanarayan90@users.noreply.github.com>
2021-07-03 17:39:41 +0530

    Add files via upload

---
+++ src/main/java/springboot/SpringSecurityWithJwdApplication.java
@@ -0,0 +1,13 @@
+package springboot;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringSecurityWithJwdApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SpringSecurityWithJwdApplication.class, args);
+	}
+
+}
